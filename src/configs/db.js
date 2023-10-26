2dd9477a7269d7996df4d17a6d292627be8179ac
mrizkysolehudin <mrizkysolehudin11@gmail.com>
2023-10-26 15:34:13 +0700

    change db config & SERVER_PORT & add vercel.json

--- src/configs/db.js
+++ src/configs/db.js
@@ -1,6 +1,22 @@
 const { Pool } = require("pg");
 require("dotenv").config();
 
+// const db = new Pool({
+// 	host: process.env.DB_HOST,
+// 	user: process.env.DB_USER,
+// 	password: process.env.DB_PASSWORD,
+// 	database: process.env.DB_NAME,
+// 	port: process.env.DB_PORT,
+// 	connectionString: process.env.PG_URL + "?sslmode=require",
+// });
+// const db = new Pool({
+// 	host: process.env.PG_HOST,
+// 	user: process.env.PG_USER,
+// 	password: process.env.PG_PASSWORD,
+// 	database: process.env.PG_DATABASE,
+// 	port: process.env.PG_PRISMA_URL,
+// 	connectionString: process.env.PG_URL + "?sslmode=require",
+// });
 const db = new Pool({
 	connectionString: process.env.PG_URL + "?sslmode=require",
 });
